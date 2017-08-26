ca4e24760dbcc2ea4095f5e39655fa1e33a6805f
Federico Esparza <federicoesparza@outlook.com>
2017-08-25 23:58:52 -0500

    Configure Rails to use SQL, instead of Ruby, for storing the schema

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,8 @@ Bundler.require(*Rails.groups)
 
 module Patron
   class Application < Rails::Application
+    config.active_record.schema_format = :sql
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
